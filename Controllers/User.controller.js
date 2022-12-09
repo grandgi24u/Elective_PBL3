1c5144216e26595c8d43fbb51292a8b24de161d9
grandgi24u <grandgirard.clement24@gmail.com>
2022-12-09 21:17:35 +0100

    ajout d'un controller + db connection + model user

---
+++ Controllers/User.controller.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+
+const User = require('../Models/User.model');
+
+module.exports = {
+  getAllUsers: async (req, res, next) => {
+    try {
+      const results = await User.find({});
+      res.send(results);
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+
+  createNewUser: async (req, res, next) => {
+    try {
+      const user = new User(req.body);
+      const result = await user.save();
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error.name === 'ValidationError') {
+        next(createError(422, error.message));
+        return;
+      }
+      next(error);
+    }
+  },
+
+  findUserById: async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const user = await User.findById(id);
+      if (!user) {
+        throw createError(404, 'Product does not exist.');
+      }
+      res.send(user);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        next(createError(400, 'Invalid Product id'));
+        return;
+      }
+      next(error);
+    }
+  },
+
+  updateAnUser: async (req, res, next) => {
+    try {
+      const id = req.params.id;
+      const updates = req.body;
+      const options = { new: true };
+
+      const result = await User.findByIdAndUpdate(id, updates, options);
+      if (!result) {
+        throw createError(404, 'Product does not exist');
+      }
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        return next(createError(400, 'Invalid Product Id'));
+      }
+
+      next(error);
+    }
+  },
+
+  deleteAnUser: async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const result = await User.findByIdAndDelete(id);
+      // console.log(result);
+      if (!result) {
+        throw createError(404, 'Product does not exist.');
+      }
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+      if (error instanceof mongoose.CastError) {
+        next(createError(400, 'Invalid Product id'));
+        return;
+      }
+      next(error);
+    }
+  }
+};
